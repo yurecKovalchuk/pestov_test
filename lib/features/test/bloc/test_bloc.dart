1ac8d2cf61863f2f3ea128ec07aeb25f3a9e4f9d
yurecKovalchuk <KovalchukYurec@hotmail.com>
2023-12-23 23:44:03 +0200

    refactor: add data source for feature test

--- lib/features/test/bloc/test_bloc.dart
+++ lib/features/test/bloc/test_bloc.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:pestov_test/data/data.dart';
 
 import 'package:pestov_test/models/models.dart';
 
@@ -9,5 +10,28 @@ part 'test_state.dart';
 part 'test_bloc.freezed.dart';
 
 class TestBloc extends Cubit<TestState> {
-  TestBloc() : super(const TestState());
+  TestBloc(this._testRepository)
+      : super(const TestState(
+          currentQuestionIndex: 0,
+        ));
+
+  final TestRepository _testRepository;
+
+  void loadQuestions() async {
+    final result = await _testRepository.loadQuestions();
+    emit(state.copyWith(questions: result));
+  }
+
+  void selectAnswer(int answerIndex) {
+    moveToNextQuestion();
+  }
+
+  void moveToNextQuestion() {
+    updateCurrentQuestionIndex();
+  }
+
+  void updateCurrentQuestionIndex() {
+    final updateQuestionIndex = state.currentQuestionIndex + 1;
+    emit(state.copyWith(currentQuestionIndex: updateQuestionIndex));
+  }
 }
--- lib/features/test/bloc/test_state.dart
+++ lib/features/test/bloc/test_state.dart
@@ -3,6 +3,8 @@ part of 'test_bloc.dart';
 @freezed
 class TestState with _$TestState {
   const factory TestState({
-    List<TestModel>? answers,
+    List<TestQuestionDTO>? questions,
+    List<TestAnswerModel>? answers,
+    required int currentQuestionIndex,
   }) = _TestState;
 }
