ee875a206e15d3375214843a27d0ed5a934c1a37
yurecKovalchuk <KovalchukYurec@hotmail.com>
2023-12-25 11:19:18 +0200

    refactor: change daracource, add fluttergen, move test_questions.json

--- lib/data/datasource/assets_datasource.dart
+++ lib/data/datasource/assets_datasource.dart
@@ -4,9 +4,11 @@ import 'package:flutter/services.dart';
 
 import 'package:pestov_test/data/data.dart';
 
+import '../../gen/assets.gen.dart';
+
 class AssetsDataSource {
   Future<List<TestQuestionDTO>> loadQuestions() async {
-    String data = await rootBundle.loadString('lib/data/assets_data/test_questions.json');
+    String data = await rootBundle.loadString(Assets.data.testQuestions);
     List<dynamic> jsonList = json.decode(data);
 
     return jsonList.map((json) => TestQuestionDTO.fromJson(json)).toList();
