2300caaec57a33e9192e5eaa17bde9289d0e9eca
yurecKovalchuk <KovalchukYurec@hotmail.com>
2024-01-03 14:45:56 +0200

    build: change app indentefication file

--- android/app/src/main/kotlin/com/webtrit/disputesolver/pestov_test/MainActivity.kt
+++ android/app/src/main/kotlin/com/webtrit/disputesolver/pestov_test/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.pestov_test
+package com.webtrit.disputesolver.pestov_test
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,7 @@ import 'app/app.dart';
 import 'bootstrap.dart';
 
 void main() async {
-  await bootstrap((serviceLocator) async => ApplicationWidget(serviceLocator: serviceLocator,));
+  await bootstrap((serviceLocator) async => ApplicationWidget(
+        serviceLocator: serviceLocator,
+      ));
 }
